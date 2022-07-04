476ca3cbee5fea308976836f184673e7697b5d30
ShinHyeonSoo <xyz1484@naver.com>
2022-07-04 14:30:59 +0900

    StageS 완성

--- Default/Stage.cpp
+++ Default/Stage.cpp
@@ -26,7 +26,7 @@ CStage::~CStage()
 void CStage::Initialize(void)
 {
 	//CLineMgr::Get_Instance()->Load_File();
-	CObjMgr::Get_Instance()->Add_Object(OBJ_PLAYER, CAbstractFactory<CPlayer>::Create());
+	CObjMgr::Get_Instance()->Add_Object(OBJ_PLAYER, CAbstractFactory<CPlayer>::Create(400.f,300.f));
 	CObjMgr::Get_Instance()->Add_Object(OBJ_MONSTER, CAbstractFactory<CObstacle>::Create(300.f, 300.f));
 
 	CObjMgr::Get_Instance()->Add_Object(OBJ_MONSTER, CAbstractFactory<CLotMonster>::Create(300.f, 300.f));
